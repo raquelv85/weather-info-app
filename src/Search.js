5c066049afe7d3ac21d4184364687a9dc343e9f3
RaquelV <raquelv85@gmail.com>
2021-09-26 20:53:14 +0200

    change names and add fetch to get info weather town

--- src/Search.js
+++ src/Search.js
@@ -6,13 +6,17 @@ function Search() {
   const [selectedOptions, setSelected] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [allOptions, setAllOptions] = useState([]);
+  const [provinces, setProvinces] = useState([])
+  const [towns, setTowns] = useState([])
 
+  const URL_PROVINCES = "https://www.el-tiempo.net/api/json/v2/provincias"
   
   const getProvinces = () => {
-    fetch("https://www.el-tiempo.net/api/json/v2/provincias")
+    fetch(URL_PROVINCES)
       .then((resp) => resp.json())
       .then((data) => {
         setAllOptions(formatProvinces(data));
+        setProvinces(formatProvinces(data))
       });
   };
 
@@ -20,41 +24,68 @@ function Search() {
     return provinces.provincias.map((item, index) => ({
       label: item.NOMBRE_PROVINCIA,
       codProv: item.CODPROV,
+      key: "prov-"+index,
+      type: "province"
     }));
   };
 
   const formatTown = (town) => {
     return town.map((item, index) => ({
       label: item.NOMBRE,
-      id: item.ID_REL,
+      id: item.COD_GEO,
+      key: "town-"+index,
+      type: "town"
     }));
   };
 
   const onChange = (selectedOptions) => {
     if(selectedOptions.length <= 2){
       setSelected(selectedOptions);
-      fetchData(selectedOptions[0].codProv)
+
+      if(selectedOptions.length === 1){
+        fetchTown(selectedOptions[0].codProv)
+      }else if(selectedOptions.length === 2 ) {
+        fetchInfo(selectedOptions[0].codProv, selectedOptions[1].id)
+      }else if(selectedOptions.length === 0){
+        getProvinces()
+      }
     }
   };
 
-  const fetchData = async (id) => {
-    fetch(`https://www.el-tiempo.net/api/json/v2/provincias/${id}/municipios`)
+  const fetchTown = async (id) => {
+    fetch(`${URL_PROVINCES}/${id}/municipios`)
       .then((resp) => resp.json())
       .then((data) => {
         setAllOptions(formatTown(data.municipios));
+        setTowns(formatTown(data.municipios))
+      });
+  };
+
+  const fetchInfo = async (idProvince, IdTown) => {
+    fetch(`${URL_PROVINCES}/${idProvince}/municipios/${IdTown}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
       });
   };
 
   const onSearchChange = (searchValue) => {
-    if (searchValue !== "" && selectedOptions < 2) {
+    let search = []
       setLoading(true);
+      if(selectedOptions.length === 0){
+        search = provinces
+      }else if(selectedOptions.length === 1){
+        search = towns
+      }
+
       setAllOptions(
-        allOptions.filter((option) =>
+        search.filter((option) =>
           option.label.toLowerCase().includes(searchValue.toLowerCase())
         )
       );
+      
       setLoading(false);
-    }
+ 
   };
 
 
